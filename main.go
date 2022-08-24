987dea258c4154a3c04e5322c20bf4fa183f9611
vijeth <vijeth2@gmail.com>
2022-08-24 10:53:57 +0530

    server

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package godiagnostics
 
 import (
 	"log"
+	"net/http"
 	"runtime"
+
+	"github.com/gorilla/mux"
 )
+
+func diagnosticsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("diagnosticsHandler")
+}
 
 func StartDiagnostics() {
 	r := runtime.NumGoroutine()
 	log.Println(r)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/godiagnostics", diagnosticsHandler)
+
+	http.ListenAndServe(":9000", router)
 }
